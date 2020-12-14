b04cc9f57391050afe67b0e98a98f323ccca2d32
Vitaly Yastremsky <vitaly@droicelabs.com>
2020-12-14 21:48:51 +0300

    manual dep conf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editdd <code>src/App.js</code> and save to reload.
+          Editdd ff <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
